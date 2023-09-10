eccdb8f3983274e3ed1dfbeae898f30216f80188
mugenShouren <ayumkd@gmail.com>
2023-09-10 22:56:41 +0530

    URL lib done

--- module15/main.go
+++ module15/main.go
@@ -1,11 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/url"
+)
 
-const url string = "https://lco.dev:3000/learn?course=golang&user=ayush"
+const theUrl string = "https://lco.dev:3000/learn?course=golang&user=ayush"
 
 func main() {
 	fmt.Println("Playing with URLs")
 
-	fmt.Println(url)
+	fmt.Println(theUrl)
+
+	res, _ := url.Parse(theUrl)
+
+	fmt.Println(res.Scheme)
+	fmt.Println(res.Host)
+	fmt.Println(res.Path)
+	fmt.Println(res.RawQuery)
+	fmt.Println(res.Port())
+
+	qparams := res.Query()
+
+	fmt.Printf("Type of params %T\n", qparams)
+
+	fmt.Printf("%T", qparams["course"])
+
+	urlConstructor := &url.URL{
+		Scheme:  "https",
+		Host:    "lco.dev",
+		Path:    "/tutcss",
+		RawPath: "course=go",
+	}
+	fmt.Println("Url Constructed", urlConstructor.String())
 }
